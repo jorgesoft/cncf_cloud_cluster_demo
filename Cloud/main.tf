f01a401d11af41f32d0e6c7a68a649862f2fd89e
Jorge Silva <47541801+jorgesoft@users.noreply.github.com>
2022-10-06 16:36:34 -0400

    remove double provider

--- Cloud/main.tf
+++ Cloud/main.tf
@@ -12,10 +12,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location
